9c777b6be0f1e79bf0056cb65a9b57ffe29a4a7b
AstonJ <email@astonj.com>
2015-05-06 20:13:08 +0100

    First thoughts on password reset form.

--- app/user_templates/tasks/user_template_tasks.rb
+++ app/user_templates/tasks/user_template_tasks.rb
@@ -1,14 +1,25 @@
 class UserTemplateTasks < Volt::Task
-  def send_reset_email(email)
+  def send_password_reset_email(email)
     # Find user by e-mail
     Volt.skip_permissions do
       store._users.where(email: email).fetch_first do |user|
         if user
-          Mailer.deliver('user_templates/mailers/forgot', {to: email, name: user._name})
+          setup_and_send_password_reset(user, email)
         else
           raise "There is no account with the e-mail of #{email}."
         end
       end
     end
+  end
+  
+  def setup_and_send_password_reset(user, email)
+    generate_token(user, :password_reset_token, true)
+    user.password_reset_sent_at = Time.zone.now
+    user.save
+    Mailer.deliver('user_templates/mailers/forgot', {to: email, name: user._name})
+  end
+  
+  def generate_token(user, column, unique=true)
+    user[column] = SecureRandom.urlsafe_base64
   end
 end
